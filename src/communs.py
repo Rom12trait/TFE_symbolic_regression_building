21a162c56cb4b91754dd32c70a3a73eb8ffd5ef9
Rom12trait <79644815+Rom12trait@users.noreply.github.com>
2026-02-25 12:21:02 +0100

    agregate et tolatex dans communs

--- src/communs.py
+++ src/communs.py
@@ -151,3 +151,63 @@ def create_run_folder(model_name, base_dir="results/"):
     os.makedirs(run_path, exist_ok=True)
     return run_path
 
+
+def agregate(runfile):
+
+    runfile = runfile
+
+    project_root = Path(__file__).resolve().parents[1]
+    run_dir = project_root / "results" / runfile
+    FILES = {
+        "Benchmark": run_dir / "metrics_benchmark_rc.xlsx",
+        "PySR": run_dir / "metrics_pysr.xlsx",
+        "RL": run_dir / "metrics_rl.xlsx",
+        "RC": run_dir / "metrics_rc.xlsx"
+    }
+    print("PROJECT_ROOT =", project_root)
+    print("RUN_DIR =", run_dir)
+
+    dfs = []
+
+    for model, path in FILES.items():
+        if not path.exists():
+            raise FileNotFoundError(f" Fichier introuvable : {path}")
+        df = pd.read_excel(path)
+        df["model"] = model
+        dfs.append(df)
+
+    final_df = pd.concat(dfs, ignore_index=True)
+
+    output = Path(run_dir / "metrics_all_models.xlsx")
+    final_df.to_excel(output, index=False)
+
+    print(f" Fichier créé : {output}")
+
+def tolatex(runfile):
+    project_root = Path(__file__).resolve().parents[1]
+    run_dir = project_root / "results" / runfile
+
+    file_path = run_dir / "metrics_all_models.xlsx"
+    df = pd.read_excel(file_path)
+    df_transposed = df.T
+    # 1. Utiliser la première ligne comme en-tête
+    df_transposed.columns = df_transposed.iloc[0]
+
+    # 2. Supprimer la première ligne (qui est maintenant dans l'en-tête)
+    # et réinitialiser l'index pour que "Models", "RC", etc. soit une colonne
+    df_final = df_transposed.drop(df_transposed.index[0]).reset_index()
+    # df_final = df_transposed.reset_index()
+
+    # 3. Optionnel : renommer la colonne d'index si nécessaire
+    df_final = df_final.rename(columns={'index': 'Modeles'})
+
+    # 2. Convertir en LaTeX
+    # index=False pour ne pas inclure la numérotation des lignes
+    latex_code = df_final.to_latex(index=False, caption="Mon tableau", label="tab:mon_tableau")
+
+    # 3. Sauvegarder dans un fichier .tex
+    with open(run_dir/"tableau.tex", "w", encoding="utf-8") as f:
+        f.write(latex_code)
+
+    print("Conversion réussie ! Fichier 'tableau.tex' généré.")
+
