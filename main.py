036c037e69f8c40042a64177f1167a3aa0a3ca09
Rom12trait <79644815+Rom12trait@users.noreply.github.com>
2026-03-10 13:01:54 +0100

    ajout run 24h pour tous

--- main.py
+++ main.py
@@ -1,24 +1,26 @@
 import numpy as np
-import pandas as pd
 import importlib
-from src import communs, pysr_model
+from src import communs, pysr_model, linear_model, function_rc
 importlib.reload(communs)
 importlib.reload(pysr_model)
-from src.function_rc import RCmodel, compute_r, slab_capacity, load_idf
+importlib.reload(linear_model)
+importlib.reload(function_rc)
+from src.function_rc import RCmodel, load_idf
 from sklearn.model_selection import train_test_split
 from src.linear_model import LinearRegressionModel
 from src.pysr_model import PySRModel
 
 
-runfile="run_annee_dyn"
+runfile="run_complet_annee_dyn"
 #run_dir = communs.create_run_folder("run_2","results")
 randomstate=42
 
 #Charger les données
 #soit airport + brussel bel ensemble ou annee dyn
 #df = communs.load_data("dataset/output_energyplus/US_SF_data_energyplus_airport_15min.csv")
-df_test = communs.load_data("dataset/output_energyplus/US_SF_data_energyplus_Brussels_bel_15min.csv")
+#df_test = communs.load_data("dataset/output_energyplus/US_SF_data_energyplus_Brussels_bel_15min.csv")
 df = communs.load_data("dataset/modèle habitation/model_annee_dynamique.csv")
+df_test = communs.load_data("dataset/modèle habitation/model_annee_dynamique_brussel_bel.csv")
 idf = load_idf(
     "dataset/modèle habitation/US+SF+CZ4C+hp+slab+IECC_2024_Brussels_airport_V2420.idf",
     "C:/Users/Corentin/energyplus/Energy+.idd"
@@ -39,57 +41,53 @@ y_hour=np.array(y_hour)
 #y_test = df_test["Tzone_next"].values
 
 #normalement x_val et y_val
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=randomstate)
+X_train, x_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=randomstate)
 
 test1 = X[:,0]
 test2 = X[:,1]
 Test3 = X[:,2]
 
 #%% RC
-
 model = RCmodel(
-    R= 0.008636689, #compute_r(idf), 0.0009
-    C= 27.14e6, #slab_capacity(), 26.82e6,
+    res= 0.008636689, #compute_r(idf), 0.0009
+    capa= 27.14e6, #slab_capacity(), 26.82e6,
     dt=900, #15 min
     random_state= randomstate
 )
-#%%
 
-T_pred, train_time_rc = communs.time_function(model.predict_free, X[:,0], X[:,1], X[:,2])
+# pas de temps 15min
+T_pred, train_time_rc = model.predict(X[:,0], X[:,1], X[:,2])
+metrics_rc = communs.compute_metrics(test1, T_pred, train_time_rc)
 
-metrics_rc = communs.compute_metrics(y, T_pred, train_time_rc)
-
-
-timestamp = pd.Timestamp.now().strftime("%Y-%m-%d_%H-%M")
 communs.save_run_to_excel(
     filepath=f"results/{runfile}/metrics_rc.xlsx",
     model_name="RC_model",
     metrics=metrics_rc,
-    comment="RC pas de temps"
+    comment="RC pas de temps 15min"
 )
 communs.save_predictions(
     filepath=f"results/{runfile}/RC_predictions.xlsx",
     datetime_index=df.index,
-    t_true=y,
+    t_true=test1,
     t_pred=T_pred
 )
 model.save_parameters(f"results/{runfile}")
 
 #%%
 
-t_pred_day = model.simulate_by_day(X[:,0], X[:,1], X[:,2])
+t_pred_day, train_time_rc_24 = model.simulate_by_day(X[:,0], X[:,1], X[:,2])
+metrics_rc_24 = communs.compute_metrics(test1, t_pred_day, train_time_rc_24)
 
-metrics_rc_day = communs.compute_metrics(y, t_pred_day)
+communs.save_run_to_excel(
+    filepath=f"results/{runfile}/metrics_rc.xlsx",
+    model_name="RC_model",
+    metrics=metrics_rc_24,
+    comment="RC déroulement 24h"
+)
 
-t_air2r2c_pred, tm_pred  = model.simulate_euler_implicite(X[:,1], X[:,2], X[0,0])
-metrics_rc_2r2c = communs.compute_metrics(y, t_air2r2c_pred)
-
-#%% benchmark 24h
-
+#%% benchmark 24h okay
 
 y_benchmark_true, y_benchmark_pred = model.benchmark(X[:,0])
-
-
 metrics_Benchmark_24h = communs.compute_metrics(y_benchmark_true, y_benchmark_pred)
 
 communs.save_run_to_excel(
@@ -99,26 +97,6 @@ communs.save_run_to_excel(
     comment="benchmark, step 15min, 1jour/4"
 )
 
-
-# 24h
-model24h = RCmodel(
-    R= 0.008636689, #compute_r(idf), 0.0009
-    C= 27.14e6, #slab_capacity(), 26.82e6,
-    dt=24*4*900, #15 min
-    random_state= randomstate
-)
-
-T_pred_24, train_time_rc_24 = communs.time_function(model24h.predict_free, X[::96,0], X[::96,1], X[::96,2])
-
-metrics_rc_24 = communs.compute_metrics(y[::96], T_pred_24, train_time_rc_24)
-
-communs.save_run_to_excel(
-    filepath=f"results/{runfile}/metrics_rc.xlsx",
-    model_name="RC_model",
-    metrics=metrics_rc_24,
-    comment="RC pas de temps 24h"
-)
-
 #%% Regression linéaire
 
 
@@ -126,9 +104,9 @@ model_rl = LinearRegressionModel(random_state=42)
 
 train_time_rl = model_rl.fit(X_train, y_train)
 
-y_pred, test_time_rl = model_rl.predict(X_test)
+y_pred, test_time_rl = model_rl.predict(x_val)
 
-metrics_rl = communs.compute_metrics(y_test, y_pred, train_time_rl, test_time_rl)
+metrics_rl = communs.compute_metrics(y_val, y_pred, train_time_rl, test_time_rl)
 
 
 model_rl.save_parameters(f"results/{runfile}/",
@@ -136,15 +114,33 @@ model_rl.save_parameters(f"results/{runfile}/",
 
 communs.save_run_to_excel(
     filepath=f"results/{runfile}/metrics_rl.xlsx",
-    model_name="regression lineaire",
+    model_name="regression linéaire",
     metrics=metrics_rl,
-    comment="rl en utilisant train_test_split, 15min"
+    comment="rl pas de temps 15min"
 )
 communs.save_predictions(
     filepath=f"results/{runfile}/rl_predictions_.xlsx",
     datetime_index=None,
-    t_true=y_test,
+    t_true=y_val,
     t_pred=y_pred
+)
+
+# rl 24h
+
+t_pred_24h, test_time_24h = model_rl.predict24hour(X[:,0], X[:,1], X[:,2])
+metrics_rl_24h = communs.compute_metrics(X[:,0], t_pred_24h, None, test_time_24h)
+
+communs.save_run_to_excel(
+    filepath=f"results/{runfile}/metrics_rl.xlsx",
+    model_name="regression linéaire",
+    metrics=metrics_rl_24h,
+    comment="rl déroulement 24H"
+)
+communs.save_predictions(
+    filepath=f"results/{runfile}/rl_24h_predictions_.xlsx",
+    datetime_index=None,
+    t_true=X[:,0],
+    t_pred=t_pred_24h
 )
 
 #%% régression symbolique
@@ -159,9 +155,13 @@ model_pysr = PySRModel(random_state=randomstate, niterations= 40)
 
 train_time_pysr = model_pysr.fit_class(X_train, y_train)
 
-y_pred_pysr, test_time_pysr = model_pysr.predict_time(X_test)
+y_pred_pysr, test_time_pysr = model_pysr.predict_time(x_val)
 
-metrics_pysr = communs.compute_metrics(y_test, y_pred_pysr, train_time_pysr, test_time_pysr)
+metrics_pysr = communs.compute_metrics(y_val, y_pred_pysr, train_time_pysr, test_time_pysr)
+
+y_pred_pysr_24h, test_time_pysr_24h = model_pysr.predict_24h(X)
+metrics_pysr_24h = communs.compute_metrics(X[:,0], y_pred_pysr_24h, None, test_time_pysr_24h)
+
 
 communs.save_run_to_excel(
     filepath=f"results/{runfile}/metrics_pysr.xlsx",
@@ -172,14 +172,29 @@ communs.save_run_to_excel(
 communs.save_predictions(
     filepath=f"results/{runfile}/pysr_predictions.xlsx",
     datetime_index=None,
-    t_true=y_test,
+    t_true=y_val,
     t_pred=y_pred_pysr
 )
 
 model_pysr.save_parameters(f"results/{runfile}/",
                           filename=f"{model_pysr.__class__.__name__}.json")
+
+communs.save_run_to_excel(
+    filepath=f"results/{runfile}/metrics_pysr.xlsx",
+    model_name="PySR_model",
+    metrics=metrics_pysr_24h,
+    comment="PySR déroulement 24h"
+)
+communs.save_predictions(
+    filepath=f"results/{runfile}/pysr_predictions_24h.xlsx",
+    datetime_index=None,
+    t_true=X[:,0],
+    t_pred=y_pred_pysr_24h
+)
+
 #%%
 communs.agregate(runfile)
+#%%
 communs.tolatex(runfile)
 
 
